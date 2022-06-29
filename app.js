38a20677fa9a6dabb80f464f6894c58a66d75bf1
asadullah696 <mr.asadullah19@gmail.com>
2022-06-30 01:53:37 +0500

    Empty java file for future update

---
+++ app.js
@@ -0,0 +1,63 @@
+var bannerData = ["one.jfif", "two.jfif", "three.jfif", "four.jfif", "five.jfif", "six.jfif",]
+var dom = document.getElementById("banner");
+function bannerInit(index){
+    var img = document.createElement('img');
+    img.setAttribute('src', "./img/" + bannerData[index]);
+    dom.innerHTML = `<img class="banner" src="./img/${bannerData[index]}"/>`
+} 
+// document.getElementById("banner").innerHTML = bannerInit
+
+
+
+var count = 0;
+setInterval(function(){
+    if (pic.lenth ===0){
+        count = 0
+    }
+    bannerData(count++)
+},1000)
+
+
+
+// var pic = new Array();
+
+// pic[0] = new Image();
+// pic[0].src = "one.jfif";
+
+// pic[1] = new Image();
+// pic[1].src = "two.jfif";
+
+// pic[2] = new Image();
+// pic[2].src = "three.jfif";
+
+// pic[3] = new Image();
+// pic[3].src = "four.jfif";
+
+// pic[4] = new Image();
+// pic[4].src = "five.jfif";
+
+// pic[5] = new Image();
+// pic[5].src = "six.jfif";
+
+// var a = 0;    
+// function next()
+//         {
+//         a = a+ 1;      
+//         if(a>3)
+//             {
+//                 a = 0;  
+//             }
+//     document.pic.src = pic[a].src;
+//     }
+// function pre()
+//         {
+//         a = a- 1;             
+//         if(a<0)
+//             {
+//                 a = 3;   
+//             }
+//     document.pic.src = pic[a].src;
+//     }
+
+
+
